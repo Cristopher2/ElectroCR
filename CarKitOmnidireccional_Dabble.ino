7515c5e544dc0eef7492beef65a867b57064ada2
Cristopher2 <cristopher533@gmail.com>
2024-06-13 08:49:07 -0600

    Update CarKitOmnidireccional_Dabble.ino

--- CarKitOmnidireccional_Dabble.ino
+++ CarKitOmnidireccional_Dabble.ino
@@ -1,74 +1,212 @@
-//#Arduino Code for Dabble
-//#Conect TX -> PIN 2, RX -> PIN 3
-
 #define CUSTOM_SETTINGS
 #define INCLUDE_GAMEPAD_MODULE
 #include <Dabble.h>
+//Set the following parameter to true if you want avoiding obstacles mode
+//Cambie el siguiente parametro por true si quieres que esquive obstaculos al moverte hacia adelante
+bool AVOID_OBST=false;
 
 
-//Código creado por Electro CR, instagram electro__cr, correo: cristopher533@gmail.com
-char t;
-const int LR = 7;
-const int LF = 6;
-const int RF = 5;
-const int RR = 4;
+//Configure THE PWM control pin
+const int PWM2A = 11;      //M1 motor
+const int PWM2B = 3;       //M2 motor
+const int PWM0A = 6;       //M3 motor
+const int PWM0B = 5;       //M4 motor
 
-void setup() {
-  pinMode(LF, OUTPUT);  //left motors  forward
-  pinMode(LR, OUTPUT);  //left motors reverse
-  pinMode(RF, OUTPUT);  //right  motors forward
-  pinMode(RF, OUTPUT);  //right motors reverse
-  //pinMode(9,OUTPUT);   //Led
-  Serial.begin(250000);      // make sure your Serial Monitor is also set at this baud rate.
-  Dabble.begin(9600);      //Enter baudrate of your bluetooth.Connect bluetooth on Bluetooth port present on evive.
+const int DIR_CLK = 4;     // Data input clock line
+const int DIR_EN = 7;      //Equip the L293D enabling pins
+const int DATA = 8;        // USB cable
+const int DIR_LATCH = 12;  // Output memory latch clock
+//Define the pin of ultrasonic obstacle avoidance sensor
+const int Trig = A2;       //A2 is defined as the pin Trig connected to the ultrasonic sensor
+const int Echo = A3;       //A3 is defined as the pin Echo connected to the ultrasonic sensor
+//Define motion state
+const int Forward = 216;    //216 save to Forward
+const int Back = 39;      //39 save to Back
+const int Left = 116;      //116 save to Left
+const int Right = 139;     //139 save to right
+const int Stop = 0;        //Parking variable
+const int L_turn = 198;    //turn left
+const int R_turn = 57;    //turn right
 
-  Serial.flush();
+// const int mytest = 1;     //test
+//Set the default speed between 1 and 255
+int Speed1 = 180;
+int Speed2 = 180;
+int Speed3 = 180;
+int Speed4 = 180;
 
-  pinMode(8, OUTPUT);  //right  motors forward
-  pinMode(6, OUTPUT);
-  //digitalWrite(8, HIGH);
-  //digitalWrite(6, HIGH);
+int distance = 0;           //Variables for storing ultrasonic sensor measurements
+char serialData;            //put the data received by the serial port in serialData
+char cmd;                   //Store bluetooth receive commands
+
+void setup() 
+{
+    Serial.begin(250000);      // make sure your Serial Monitor is also set at this baud rate.
+    Dabble.begin(9600,9,10);      //Enter baudrate of your bluetooth.Connect bluetooth on Bluetooth port present on evive.
+
+    Serial.flush();
+
+    //Configure as output mode
+    pinMode(DIR_CLK,OUTPUT);
+    pinMode(DATA,OUTPUT);
+    pinMode(DIR_EN,OUTPUT);
+    pinMode(DIR_LATCH,OUTPUT);
+    pinMode(PWM0B,OUTPUT);
+    pinMode(PWM0A,OUTPUT);
+    pinMode(PWM2A,OUTPUT);
+    pinMode(PWM2B,OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    pinMode(Trig,OUTPUT);//The Trig pin connected to the ultrasound is set to output mode
+    pinMode(Echo,INPUT);//The Echo pin connected to the ultrasound is set to input mode
 }
 
-void loop() {
-  Dabble.processInput();             //this function is used to refresh data obtained from smartphone.Hence calling this function is mandatory in order to get data properly from your mobile.
-  Serial.print("KeyPressed: ");
+void loop()
+{
+    distance = SR04(Trig,Echo);       //Acquisition of ultrasonic distance
+    control_func(); //Call the Bluetooth car control function
+//    Motor(mytest,Speed1,Speed1,Speed1,Speed1);//test
+}
+
+/* Function name: Motor();
+* Function: Change the movement direction and speed of the car through the entrance parameters
+* Entry parameter 1: Dri car movement direction
+* Entry parameters 2~3: Speed1~Speed4 motor speed, value range 0~255
+* Dri value description (forward :216; Back: 39; Left translation: 116; Right translation: 139; Stop: 0;
+* Right rotation: 198; Left rotation: 57)
+* Return value: None
+ */
+void Motor(int Dir,int Speed1,int Speed2,int Speed3,int Speed4)
+{
+    analogWrite(PWM2A,Speed1); //Motor PWM speed regulation
+    analogWrite(PWM2B,Speed2); //Motor PWM speed regulation
+    analogWrite(PWM0A,Speed3); //Motor PWM speed regulation
+    analogWrite(PWM0B,Speed4); //Motor PWM speed regulation
     
-  if (GamePad.isUpPressed()) {          //move  forward(all motors rotate in forward direction)
-    digitalWrite(LF, HIGH);
-    digitalWrite(RF, HIGH);
-    Serial.println("1");
-  }
-
-  else if (GamePad.isDownPressed()) {    //move reverse (all  motors rotate in reverse direction)
-    digitalWrite(LR, HIGH);
-    digitalWrite(RR, HIGH);
-    Serial.println("2");
-
-  }
-
-  else if (GamePad.isRightPressed()) {    //turn right (left side motors rotate in forward direction,  right side motors doesn't rotate)
-    digitalWrite(LF, HIGH);
-    digitalWrite(RR, HIGH);
-    Serial.println("3");
-
-  }
-
-  else  if (GamePad.isLeftPressed()) {    //turn left (right side motors rotate in forward direction, left  side motors doesn't rotate)
-    digitalWrite(LR, HIGH);
-    digitalWrite(RF, HIGH);
-    Serial.println("4");
-
-  }
-
-  else {    //STOP (all motors stop)
-    digitalWrite(RF, LOW);
-    digitalWrite(LF, LOW);
-    digitalWrite(RR, LOW);
-    digitalWrite(LR, LOW);
-    Serial.println("5");
-
-  }
-  delay(10);
+    digitalWrite(DIR_LATCH,LOW); //DIR_LATCH sets the low level and writes the direction of motion in preparation
+    shiftOut(DATA,DIR_CLK,MSBFIRST,Dir);//Write Dir motion direction value
+    digitalWrite(DIR_LATCH,HIGH);//DIR_LATCH sets the high level and outputs the direction of motion
 }
 
+/*
+Function name: SR04()
+Function: Obtain ultrasonic ranging data
+Entry parameters: Trig, Echo
+Function return value: cm
+*/
+int SR04(int Trig,int Echo)
+{
+    digitalWrite(Trig,LOW);     //Trig is set to low level
+    delayMicroseconds(2);       //Wait 2 microseconds
+    digitalWrite(Trig,HIGH);    //Trig is set to high level
+    delayMicroseconds(10);      //Wait 15 microseconds
+    digitalWrite(Trig,LOW);     //Trig is set to low
+
+    float cm = pulseIn(Echo,HIGH)/58; //Convert the ranging time to CM
+    //Serial.print("D");    //Character Distance displayed in serial port monitor window:
+    //Serial.println(cm);   
+    return cm;      //Returns cm value ranging data
+}
+/*
+* Function name: control_func()
+* Function: Receive Bluetooth data, control the car movement direction
+* Entry parameters: None
+* Return value: None
+*/
+void control_func()
+{
+   // Serial.print("T");    //Character Distance displayed in serial port monitor window:
+    //Serial.println(Speed1);
+    Dabble.processInput();             //this function is used to refresh data obtained from smartphone.Hence calling this function is mandatory in order to get data properly from your mobile.
+
+      if(GamePad.isLeftPressed()&&GamePad.isUpPressed())  cmd = 'Z';
+      else if(GamePad.isDownPressed()&&GamePad.isRightPressed() )  cmd = 'E';
+      else  if (GamePad.isUpPressed())  cmd = 'F';     //If the data received by the serial port is character F, save F to CMD
+      else if (GamePad.isDownPressed())  cmd = 'B';     //If the data received by the serial port is character B, save F to CMD
+      else if(GamePad.isLeftPressed())  cmd = 'L';     //If the serial port receives data as the character L, save F to CMD
+      else if (GamePad.isRightPressed())  cmd = 'R';     //If the serial port receives data as the character Y, save F to CMD
+      else if(GamePad.isSelectPressed())  cmd = 'M';
+      else if( GamePad.isCirclePressed() && Speed1 < 245)//If you receive a string plus, the speed increases 
+      {
+          Speed1 += 10;   //We're going to increase the velocity by 10 at a time
+          Speed2 = Speed1;
+          Speed3 = Speed1;
+          Speed4 = Speed1;
+          //Serial.println("Speed: ");
+          //Serial.println(Speed1);
+
+      }
+      else if(GamePad.isSquarePressed() && Speed1 > 30)//When I receive a string -- the speed decreases
+      {
+          Speed1 -= 10;   //I'm going to subtract 10 at a time
+          Speed2 = Speed1;
+          Speed3 = Speed1;
+          Speed4 = Speed1;    
+          //Serial.println("Speed: ");    
+          //Serial.println(Speed1);
+
+      }
+      else {cmd = 'S';}     //If the serial port receives data as character S, save F to CMD
+      Serial.println(serialData);
+  
+    if('F' == cmd)   //If Bluetooth receives the string F, the dolly moves forward and enables obstacle avoidance
+    {      
+       if(AVOID_OBST==true){
+          AvoidingObstacles();//
+       }else{
+          Motor(Forward,Speed1,Speed2,Speed3,Speed4);
+        
+       }
+    }
+    else if('G' == cmd)     //Bluetooth receives string G, car backs up
+    {   
+        Motor(Back,Speed1,Speed2,Speed3,Speed4);
+    }
+    else if('L' == cmd)    //Bluetooth received the string L, car left translation
+    {              
+        Motor(Left,Speed1,Speed2,Speed3,Speed4); 
+    }
+    else if('R' == cmd)     //Bluetooth received the string R
+    {
+        Motor(Right,Speed1,Speed2,Speed3,Speed4);      //right translation   
+    }
+    else if('S' == cmd)      //When the string S is received, the cart stops moving
+    {
+        Motor(Stop,0,0,0,0);
+    }
+    else if('Z' == cmd)     //Bluetooth received the string Z
+    {
+        Motor(L_turn,Speed1,Speed2,Speed3,Speed4);      //turn left   
+    }
+    else if('E' == cmd)     //Bluetooth received the string E
+    {
+        Motor(R_turn,Speed1,Speed2,Speed3,Speed4);      //turn right   
+    }
+    else if('M' == cmd)     //Bluetooth received the string M
+    {
+      AVOID_OBST = !AVOID_OBST;   
+      digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) ^ 1);
+      //Serial.println(AVOID_OBST);
+      cmd = 'S';
+
+    }
+}
+
+void AvoidingObstacles()
+{
+    if((distance > 20 ) || cmd == 'F')//If the distance is greater than 20cm or bluetooth receives a command equal to F
+    {
+        delay(100);//Delay of 100 ms
+        if(distance > 20)//Again, determine if the distance is really greater than 20cm
+        {
+            Motor(Forward,Speed1,Speed2,Speed3,Speed4); //Call forward function
+        }
+        else //Otherwise the distance is less than 20
+        {
+            Motor(Back,Speed1,Speed2,Speed3,Speed4);//retreat
+            delay(500);
+            Motor(Left,Speed1,Speed2,Speed3,Speed4);//Turn left to change the direction of the car
+            delay(500);
+        }
+    }
+}
